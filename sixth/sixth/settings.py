63ba9645889325b202242599cf6e33c4cf73297a
Mohammed Shefeeq <ems.shafeeq@gmail.com>
2018-09-24 08:07:33 +0530

    edited settings

--- sixth/sixth/settings.py
+++ sixth/sixth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y_@zsu*zc8tfgp-9e!ycbilk6$d7fcxv#mkg28-w2*hdlc=$-n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
